88da3eb5de6ff622ba78e27be0457e2160a40f56
ram_2219 <r.morr1922@gmail.com>
2019-10-01 17:59:55 -0500

    created a base angular project

---
+++ tvs_ng/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { ForumComponent } from './forum/forum.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { SearchComponent } from './search/search.component';
+import { UpdateAccountComponent } from './update-account/update-account.component';
+import { VetdecComponent } from './vetdec/vetdec.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    ForumComponent,
+    CreateAccountComponent,
+    SearchComponent,
+    UpdateAccountComponent,
+    VetdecComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
