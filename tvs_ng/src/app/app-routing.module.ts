8d968cb0da0cd8506a61dee8f367831434784379
Anthony Visuano <anthony.visuano@gmail.com>
2019-10-02 17:37:15 -0500

    was playing with search, but need to get the database/java working

--- tvs_ng/src/app/app-routing.module.ts
+++ tvs_ng/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "search", 
+    component: SearchComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
