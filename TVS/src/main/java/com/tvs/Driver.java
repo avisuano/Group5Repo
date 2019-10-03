4f5451710ceb332230c16608883047e95ea54f52
leahmedina <leahmedina.lm@gmail.com>
2019-10-03 17:41:04 -0500

    Implemented Forum DOAs and adjusted User's

--- TVS/src/main/java/com/tvs/Driver.java
+++ TVS/src/main/java/com/tvs/Driver.java
@@ -1,35 +1,77 @@
 package com.tvs;
 
+import com.tvs.model.Forum;
 import com.tvs.model.User;
+import com.tvs.service.ForumService;
 import com.tvs.service.UserService;
 
 public class Driver {
 	
 	public static void main(String[] args) {
+		
+		/*
+		 * CALLING USER LOGIC (TO MAKE SURE IT ALL WORKS)
+		 * ***********************************************
+		 */
+		
 		UserService us = new UserService();
 		//Getting list of users
-		System.out.println(us.getAllUsers()); 
+//		System.out.println(us.getAllUsers()); 
+//		
+//		//Creating new user
+//		User newUser = new User(4, "newuser", "newuser@mail.com", "Jane", "Doe", false, 0);
+//		us.insertUser(newUser);
+//		
+//		System.out.println(us.getAllUsers()); 
+//		
+//		//Getting user by ID
+//		System.out.println(us.getUserById(2));
+//		
+//		//Getting user by name
+//		System.out.println(us.getUserByName("Robert"));
+//		
+//		//Updating User
+//		User newUserUpdate = new User(4, "Jane4321", "jane@mail.com", "Jane", "Doeth", false, 0);
+//		us.updateUser(newUserUpdate);
+//		
+//		//Deleting User by user name
+//		System.out.println(us.getAllUsers()); 
+//		us.deleteUser("Jane4321");
+//		System.out.println(us.getAllUsers()); 
 		
-		//Creating new user
-		User newUser = new User(4, "newuser", "newuser@mail.com", "Jane", "Doe", false, 0);
-		us.insertUser(newUser);
+		/*
+		 * CALLING FORUM LOGIC (TO MAKE SURE IT ALL WORKS)
+		 * ***********************************************
+		 */
 		
-		System.out.println(us.getAllUsers()); 
+		ForumService fs = new ForumService();
+		System.out.println(fs.getAllForums());
 		
-		//Getting user by ID
-		System.out.println(us.getUserById(2));
+		//Creating new forum
+		Forum newForum = new Forum(6, "New Forum", "This is a new forum body", false, 0, 2);
+		fs.insertForum(newForum);
 		
-		//Getting user by name
-		System.out.println(us.getUserByName("Robert"));
+		System.out.println(fs.getAllForums());
 		
-		//Updating User
-		User newUserUpdate = new User(4, "Jane4321", "jane@mail.com", "Jane", "Doeth", false, 0);
-		us.updateUser(newUserUpdate);
+		//Getting forum by ID
+		System.out.println(fs.getForumById(2));
 		
-		//Deleting User by user name
-		System.out.println(us.getAllUsers()); 
-		us.deleteUser("Jane4321");
-		System.out.println(us.getAllUsers()); 
+		//Getting forum by title (or name..whichever you wanna call it)
+		System.out.println(fs.getForumByName("What happens after the military?"));
 		
+		//Updating Forum
+		Forum newForumUpdate = new Forum(5, "How do I convert my military skills to civilian skills?", 
+				"After getting out the military I'm struggling with converting"
+				+ " my military skills/experiences to a position in the civilian world.", 
+				false, 0, 1);
+		fs.updateForum(newForumUpdate);
+		
+		System.out.println(fs.getAllForums());
+		
+		//Deleting forum by title
+		System.out.println(fs.getAllForums()); 
+		fs.deleteForum("How do I convert my military skills to civilian skills?");
+		System.out.println(fs.getAllForums()); 
+	
 	}
 }
---
+++ TVS/src/main/java/com/tvs/repository/ForumRepository.java
@@ -0,0 +1,13 @@
+package com.tvs.repository;
+
+import java.util.List;
+import com.tvs.model.Forum;
+
+public interface ForumRepository {
+	public List<Forum> getAllForums(); // Get all forums
+	void insertForum(Forum f); // Insert new forum
+	Forum getForumById(int id); // Get forum by ID
+	Forum getForumByName(String title); // Get forum by title
+	void updateForum(Forum f); // Update forum information
+	void deleteForum(String title); // Delete forum by title
+}
