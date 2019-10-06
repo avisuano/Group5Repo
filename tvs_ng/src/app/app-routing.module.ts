852f2a4722d65fd505a3832067becdad18cd4491
leahmedina <leahmedina.lm@gmail.com>
2019-10-05 22:54:33 -0500

    Adding Forum feature

--- tvs_ng/src/app/app-routing.module.ts
+++ tvs_ng/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { SearchComponent } from './search/search.component';
+import { ForumComponent } from './forum/forum.component';
+import { VetdecComponent } from './vetdec/vetdec.component';
 
 
 const routes: Routes = 
@@ -20,6 +22,18 @@ const routes: Routes =
     path: "create_account",
     component: CreateAccountComponent
   },
+  {
+    path: "forum",
+    component: ForumComponent
+  },
+  {
+    path: "search",
+    component: SearchComponent
+  },
+  {
+    path: "vetdec",
+    component: VetdecComponent
+  }
 ];
 
 @NgModule({
