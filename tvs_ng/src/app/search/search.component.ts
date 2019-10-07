d781b90a75903a5daa7819a91431ac43f3ebf814
Anthony <avisuano15@gmail.com>
2019-10-07 15:57:38 -0500

    saving some progress

--- tvs_ng/src/app/search/search.component.ts
+++ tvs_ng/src/app/search/search.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -20,13 +19,8 @@ export class SearchComponent implements OnInit {
   getAdr(string: any) {
     let adr = this.address
     let addressUrl = adr.split(' ').join('+')
-    let result = this.getGeo(addressUrl)
-    console.log(result)
+    let qurl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + addressUrl + '&key=AIzaSyBERe0UJKwjez5wZByGBUoEAQx0cy67vEk'
+    console.log(qurl)
   }
 
-  getGeo(url){
-    let qurl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + url + '&key=AIzaSyBERe0UJKwjez5wZByGBUoEAQx0cy67vEk'
-    let add = this.http.get(qurl) as Observable<FormData>
-    console.log(add)
-  }
 }
