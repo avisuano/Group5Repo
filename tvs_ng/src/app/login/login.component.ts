a41fcd0291bd051fb6213ea42d19131bff833164
ram_2219 <r.morr1922@gmail.com>
2019-10-09 17:22:14 -0500

    vetdec

--- tvs_ng/src/app/login/login.component.ts
+++ tvs_ng/src/app/login/login.component.ts
@@ -12,4 +12,8 @@ export class LoginComponent implements OnInit
   {
     
   }
+  userLogin()
+  {
+    let i:number = 0;
+  }
 }
--- tvs_ng/src/app/services/user.service.ts
+++ tvs_ng/src/app/services/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpParams, HttpClient } from '@angular/common/http';
+import { User} from '../classes/User';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService 
+{
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  userLogin(user:User){
+    let headers = new HttpHeaders().set("Content-Type", "application/x-www-form-urlencoded");
+
+    let body = new HttpParams().set("username", user.username.toString()).set("password", user.password.toString());
+    return this.http.post("http://localhost:8080/SpringMVC/user/login", {headers: headers});
+  }
 }
