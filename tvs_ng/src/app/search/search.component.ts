51aef7b022304e02f85f5106e504616ea250eb9d
Anthony Visuano <avisuano15@wou.edu>
2019-10-10 02:40:14 -0500

    shipping changes to laptop

--- tvs_ng/src/app/search/search.component.ts
+++ tvs_ng/src/app/search/search.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { SearchService } from '../services/search.service';
-import { google } from '@agm/core/services/google-maps-types';
+
+declare var google: any
 
 @Component({
   selector: 'app-search',
@@ -10,37 +11,38 @@ import { google } from '@agm/core/services/google-maps-types';
 export class SearchComponent implements OnInit {
 
   private address: ''
-  private lat = 44.9428975;
-  private lng = -123.0350963;
+  public lat = 44.8484524
+  public lng = -123.23398880000002
 
-  constructor(private ss:SearchService) {
+  constructor(private ss: SearchService, private _zone: NgZone) {
   }
 
   ngOnInit() {
-    this.getAdr(this.address)
   }
 
   getAdr(address: string) {
+
+    let x = document.getElementById('map');
+    x.style.display = "none";
+
+    // Well geo coder kind of works
+    let geocoder = new google.maps.Geocoder()
+    geocoder.geocode({ 'address': this.address }, function (results) {
+      this.lat = results[0].geometry.location.lat()
+      this.lng = results[0].geometry.location.lng()
+      console.log('Inside the function ' + this.lat + ', ' + this.lng)
+    })
+    console.log('After the function ' + this.lat + ', ' + this.lng)
+
     // Remove the whitespace and replace with +'s
     let url = this.address.split(' ').join('+')
     // Build the complete url to send to Google Geocode to get the latitude and longitude
     let qurl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + url + '&key=AIzaSyBERe0UJKwjez5wZByGBUoEAQx0cy67vEk'
     // Can I get an api call?
-    this.ss.getAdr(qurl).subscribe( data => { 
-      console.log(data)
-    })
+    this.ss.getAdr(qurl).subscribe(data => { console.log(data) })
 
-    let geocoder = new google.maps.Geocoder()
-    geocoder.geocde({'address' : this.address}, function(results, status){
-      if(status === google.maps.GeocoderStatus.OK){
-        this.lat = results[0].geometry.location.lat()
-        this.lng = results[0].geometry.location.lng()
-        console.log(this.lat + ', ' + this.lng)
-      }
-    })
+    // Get the div container to allow it to be displayed
 
-    // Display the map
-    var x = document.getElementById("map");
     x.style.display = "block";
   }
 }
