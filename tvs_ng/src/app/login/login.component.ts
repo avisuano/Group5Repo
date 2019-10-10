326a2f0c52482d18ed1e36910371b77bd4d3875f
leahmedina <leahmedina.lm@gmail.com>
2019-10-10 10:28:22 -0500

    Updating CURRENT version of application, Angular side and Spring side

--- tvs_ng/src/app/login/login.component.ts
+++ tvs_ng/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { User } from '../classes/User';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +13,30 @@ export class LoginComponent implements OnInit
   ngOnInit() 
   {
   }
+
+  data:User[] = [];
+  
   userLogin()
   {
+    this.usr.getAllUsers().subscribe(
+
+      data => {
+        this.data = data;
+        console.log(this.data);
+      },
+      error => {
+        error = "Sorry, Couldn't load the newest forums!"
+        console.log(error);
+      }
+
+    )
+    console.log("we made it!")
     let username = (<HTMLInputElement>document.getElementById("username")).value;
     let password = (<HTMLInputElement>document.getElementById("password")).value;
 
+    console.log(username + " " + password)
     this.usr.userLogin(username,password);
+
   }
+
 }
